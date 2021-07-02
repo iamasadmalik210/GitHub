ab19a3ae8b29813e7f01e5170bbdc26a0bea1cc5
Asad Malik <iamasadmalik210@gmail.com>
2021-07-02 12:04:58 +0500

    changed background colour to green

--- GitHub/ViewController.swift
+++ GitHub/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         
-        view.backgroundColor = .red
+        view.backgroundColor = .green
+        
     }
 
 
